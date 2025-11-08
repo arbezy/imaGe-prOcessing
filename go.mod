f20e312777721ea6d99e31102051ea148ad31e99
Andrew Ellison <71777978+arbezy@users.noreply.github.com>
2025-11-08 15:12:04 +0000

    impl: image brightness adjusting

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module image-processing
 
 go 1.25.3
+
+require golang.org/x/sync v0.18.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"io"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
 )
 
-func main() {
-	readImage("images/obaa_image.jpg")
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
 }
 
-func readImage(filepath string) {
+type ImageProcessor struct {
+	pixelMap [][]Pixel
+	lock     *semaphore.Weighted
+}
+
+func readImage(filepath string) [][]Pixel {
 	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
 
 	file, err := os.Open("./" + filepath)
@@ -31,7 +46,7 @@ func readImage(filepath string) {
 		os.Exit(1)
 	}
 
-	fmt.Println(pixels)
+	return pixels
 }
 
 func getPixels(file io.Reader) ([][]Pixel, error) {
@@ -61,9 +76,55 @@ func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
 	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
 }
 
-type Pixel struct {
-	R int
-	G int
-	B int
-	A int
+func increasePixelBrightness(pixel *Pixel, value float32) {
+	pixel.R = min(255, int(float32(pixel.R)*value))
+	pixel.G = min(255, int(float32(pixel.G)*value))
+	pixel.B = min(255, int(float32(pixel.B)*value))
+}
+
+func (ip *ImageProcessor) increaseImageBrightness(value float32) {
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	fmt.Println("increasing image brightness")
+
+	for i := 0; i < len(ip.pixelMap); i++ {
+		for j := 0; j < len(ip.pixelMap[0]); j++ {
+			wg.Add(1)
+			ip.lock.Acquire(context.TODO(), 1)
+
+			go func(pixel *Pixel) {
+				defer ip.lock.Release(1)
+				defer wg.Done()
+				increasePixelBrightness(pixel, value)
+			}(&ip.pixelMap[i][j])
+		}
+	}
+}
+
+func Ulimit() int64 {
+	out, err := exec.Command("ulimit", "-n").Output()
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := strings.TrimSpace(string(out))
+	i, err := strconv.ParseInt(s, 10, 64)
+
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func main() {
+	image := readImage("images/obaa_image.jpg")
+	fmt.Println("0,0 pixel value before:", image[0][0])
+	ip := &ImageProcessor{
+		pixelMap: image,
+		lock:     semaphore.NewWeighted(Ulimit()),
+	}
+	ip.increaseImageBrightness(1.1)
+	fmt.Println("0,0 pixel value after:", ip.pixelMap[0][0])
 }
